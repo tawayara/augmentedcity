c25070c81c4fe5f5d9013b99ce13620eeddb3eed
Tiana <tiana@new-host-12.home>
2013-12-29 20:42:08 -0300

    initial code version

---
+++ code/augmented-city/src/com/tawayara/augmentedcity/MainActivity.java
@@ -0,0 +1,124 @@
+package com.tawayara.augmentedcity;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Debug;
+import android.view.Menu;
+import android.view.SurfaceHolder;
+
+import com.tawayara.augmentedcity.renderer.LightingRenderer;
+import com.tawayara.augmentedcity.renderer.Model3D;
+import com.tawayara.augmentedcity.renderer.models.Model;
+import com.tawayara.augmentedcity.renderer.parser.ObjParser;
+import com.tawayara.augmentedcity.renderer.parser.ParseException;
+import com.tawayara.augmentedcity.renderer.utils.AssetsFileUtil;
+import com.tawayara.augmentedcity.renderer.utils.BaseFileUtil;
+
+import edu.dhbw.andar.ARToolkit;
+import edu.dhbw.andar.AndARActivity;
+import edu.dhbw.andar.Config;
+import edu.dhbw.andar.exceptions.AndARException;
+
+public class MainActivity extends AndARActivity implements
+		SurfaceHolder.Callback {
+
+	private Model model;
+	private Model3D model3d;
+	private ProgressDialog waitDialog;
+	ARToolkit artoolkit;
+
+	public MainActivity() {
+		super(false);
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		super.setNonARRenderer(new LightingRenderer());// or might be omited
+		artoolkit = getArtoolkit();
+		// getSurfaceView().setOnTouchListener(new TouchEventHandler());
+		getSurfaceView().getHolder().addCallback(this);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public void uncaughtException(Thread arg0, Throwable arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void surfaceCreated(SurfaceHolder holder) {
+		super.surfaceCreated(holder);
+		// load the model
+		// this is done here, to assure the surface was already created, so that
+		// the preview can be started
+		// after loading the model
+		if (model == null) {
+			waitDialog = ProgressDialog.show(this, "",
+					getResources().getText(R.string.loading), true);
+			waitDialog.show();
+			new ModelLoader().execute();
+		}
+	}
+
+	private class ModelLoader extends AsyncTask<Void, Void, Void> {
+
+		@Override
+		protected Void doInBackground(Void... params) {
+
+			String modelFileName = "superman.obj";
+			BaseFileUtil fileUtil = null;
+			fileUtil = new AssetsFileUtil(getResources().getAssets());
+			fileUtil.setBaseFolder("models/");
+
+			// read the model file:
+			if (modelFileName.endsWith(".obj")) {
+				ObjParser parser = new ObjParser(fileUtil);
+				try {
+					if (fileUtil != null) {
+						BufferedReader fileReader = fileUtil
+								.getReaderFromName(modelFileName);
+						if (fileReader != null) {
+							model = parser.parse("Model", fileReader);
+							model3d = new Model3D(model);
+						}
+					}
+					if (Config.DEBUG)
+						Debug.stopMethodTracing();
+				} catch (IOException e) {
+					e.printStackTrace();
+				} catch (ParseException e) {
+					e.printStackTrace();
+				}
+			}
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(Void result) {
+			super.onPostExecute(result);
+			waitDialog.dismiss();
+
+			// register model
+			try {
+				if (model3d != null)
+					artoolkit.registerARObject(model3d);
+			} catch (AndARException e) {
+				e.printStackTrace();
+			}
+			startPreview();
+		}
+	}
+
+}
---
+++ code/augmented-city/src/com/tawayara/augmentedcity/SplashActivity.java
@@ -0,0 +1,37 @@
+package com.tawayara.augmentedcity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash);
+
+		new StartupTask().execute();
+	}
+
+	private class StartupTask extends AsyncTask<Void, Void, Void> {
+
+		@Override
+		protected Void doInBackground(Void... params) {
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+			intent.setAction(Intent.ACTION_VIEW);
+			startActivity(intent);
+
+			return null;
+		}
+
+	}
+}
