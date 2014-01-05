00d32fe966e6a06702d8c8b3009db0dc15fb24bc
edwincarlo <edwin.carlo@gmail.com>
2014-01-05 18:00:09 -0300

    Extracting the view creation from the activity

--- code/andar/src/edu/dhbw/andar/AndARActivity.java
+++ code/andar/src/edu/dhbw/andar/AndARActivity.java
@@ -19,197 +19,163 @@
  */
 package edu.dhbw.andar;
 
-import java.io.IOException;
 import java.lang.Thread.UncaughtExceptionHandler;
 
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
-import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.os.Debug;
 import android.view.SurfaceHolder;
 import android.view.SurfaceHolder.Callback;
-import android.view.SurfaceView;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.FrameLayout;
-import edu.dhbw.andar.camera.CameraManager;
-import edu.dhbw.andar.exceptions.AndARRuntimeException;
 import edu.dhbw.andar.interfaces.OpenGLRenderer;
-import edu.dhbw.andar.util.IO;
 
-public abstract class AndARActivity extends Activity implements Callback, UncaughtExceptionHandler{
-	private GLSurfaceView glSurfaceView;
-	private AndARRenderer renderer;
-	private Resources res;
-	private ARToolkit artoolkit;
-	private CameraStatus camStatus = new CameraStatus();
-	private PreviewSurfaceView previewSurface;
-	private CameraManager cameraManager;
-	
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Thread.currentThread().setUncaughtExceptionHandler(this);
-        res = getResources();
-        
-        artoolkit = new ARToolkit(res, getFilesDir());
-        setFullscreen();
-        disableScreenTurnOff();
-        //orientation is set via the manifest
-        
-        try {
-			IO.transferFilesToPrivateFS(getFilesDir(),res);
-		} catch (IOException e) {
-			e.printStackTrace();
-			throw new AndARRuntimeException(e.getMessage());
-		}
-		FrameLayout frame = new FrameLayout(this);
-		cameraManager = new CameraManager();
-		previewSurface = new PreviewSurfaceView(this, cameraManager);
-				
-        glSurfaceView = new GLSurfaceView(this);
-		renderer = new AndARRenderer(artoolkit);
-        glSurfaceView.setRenderer(renderer);
-        glSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
-        glSurfaceView.getHolder().addCallback(this);
-        
-        cameraManager.setPreviewHandler(new CameraPreviewHandler(glSurfaceView, renderer, res, artoolkit, camStatus));
-        
-        frame.addView(glSurfaceView);
-        frame.addView(previewSurface);
-        
-        setContentView(frame);
-        if(Config.DEBUG)
-        	Debug.startMethodTracing("AndAR");
-    }
-    
-    
-    /**
-     * Set a renderer that draws non AR stuff. Optional, may be set to null or omited.
-     * and setups lighting stuff.
-     * @param customRenderer
-     */
-    public void setNonARRenderer(OpenGLRenderer customRenderer) {
-		renderer.setNonARRenderer(customRenderer);
+public abstract class AndARActivity extends Activity implements Callback, UncaughtExceptionHandler {
+
+	private AndARView andarView;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		//Thread.currentThread().setUncaughtExceptionHandler(this);
+		setFullscreen();
+		disableScreenTurnOff();
+		// orientation is set via the manifest
+
+		andarView = new AndARView();
+		setContentView(andarView.createView(this, this));
+
+		if (Config.DEBUG)
+			Debug.startMethodTracing("AndAR");
 	}
 
-    /**
-     * Avoid that the screen get's turned off by the system.
-     */
+	/**
+	 * Set a renderer that draws non AR stuff. Optional, may be set to null or omited. and setups
+	 * lighting stuff.
+	 * 
+	 * @param customRenderer
+	 */
+	public void setNonARRenderer(OpenGLRenderer customRenderer) {
+		andarView.setNonARRenderer(customRenderer);
+	}
+
+	/**
+	 * Avoid that the screen get's turned off by the system.
+	 */
 	public void disableScreenTurnOff() {
-    	getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
-    			WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-    }
-    
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
+				WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+	}
+
 	/**
 	 * Set's the orientation to landscape, as this is needed by AndAR.
 	 */
-    public void setOrientation()  {
-    	setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-    }
-    
-    /**
-     * Maximize the application.
-     */
-    public void setFullscreen() {
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-    }
-   
-    public void setNoTitle() {
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-    } 
-    
-    @Override
-    protected void onPause() {
-        cameraManager.pause();
-        this.glSurfaceView.onPause();
-        super.onPause();
-        finish();
-    }
-    
-    @Override
-    protected void onDestroy() {
-    	super.onDestroy();    	
-    	System.runFinalization();
-    	if(Config.DEBUG)
-    		Debug.stopMethodTracing();
-    }
-    
-    
-
-    @Override
-    protected void onResume() {
-    	cameraManager.resume();
-    	glSurfaceView.onResume();
-        super.onResume();
-    }
-    
-    protected void startPreview() {
-    	previewSurface.startPreview();
-    }
-    
-    /* (non-Javadoc)
-     * @see android.app.Activity#onStop()
-     */
-    @Override
-    protected void onStop() {
-    	super.onStop();
-    }
-
-	/* The GLSurfaceView changed
-	 * @see android.view.SurfaceHolder.Callback#surfaceChanged(android.view.SurfaceHolder, int, int, int)
-	 */
-	@Override
-	public void surfaceChanged(SurfaceHolder holder, int format, int width,
-			int height) {
-		
+	public void setOrientation() {
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 	}
 
-	/* The GLSurfaceView was created
-	 * The camera will be opened and the preview started 
+	/**
+	 * Maximize the application.
+	 */
+	public void setFullscreen() {
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+	}
+
+	public void setNoTitle() {
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+	}
+
+	@Override
+	protected void onPause() {
+		andarView.pause();
+		super.onPause();
+		finish();
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		System.runFinalization();
+		if (Config.DEBUG)
+			Debug.stopMethodTracing();
+	}
+
+	@Override
+	protected void onResume() {
+		andarView.resume();
+		super.onResume();
+	}
+
+	protected void startPreview() {
+		andarView.startPreview();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.app.Activity#onStop()
+	 */
+	@Override
+	protected void onStop() {
+		super.onStop();
+	}
+
+	/*
+	 * The GLSurfaceView changed
+	 * 
+	 * @see android.view.SurfaceHolder.Callback#surfaceChanged(android.view.SurfaceHolder, int, int,
+	 * int)
+	 */
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+	}
+
+	/*
+	 * The GLSurfaceView was created The camera will be opened and the preview started
+	 * 
 	 * @see android.view.SurfaceHolder.Callback#surfaceCreated(android.view.SurfaceHolder)
 	 */
 	@Override
 	public void surfaceCreated(SurfaceHolder holder) {
-		previewSurface.setSurfaceCreated(true);
+		andarView.setSurfaceCreated(true);
 	}
 
-	/* GLSurfaceView was destroyed
-	 * The camera will be closed and the preview stopped.
+	/*
+	 * GLSurfaceView was destroyed The camera will be closed and the preview stopped.
+	 * 
 	 * @see android.view.SurfaceHolder.Callback#surfaceDestroyed(android.view.SurfaceHolder)
 	 */
 	@Override
 	public void surfaceDestroyed(SurfaceHolder holder) {
 
 	}
-	
+
 	/**
-	 * @return  a the instance of the ARToolkit.
+	 * @return a the instance of the ARToolkit.
 	 */
 	public ARToolkit getArtoolkit() {
-		return artoolkit;
-	}	
-	
+		return andarView.getArtoolkit();
+	}
+
 	/**
 	 * Take a screenshot. Must not be called from the GUI thread, e.g. from methods like
 	 * onCreateOptionsMenu and onOptionsItemSelected. You have to use a asynctask for this purpose.
+	 * 
 	 * @return the screenshot
 	 */
 	public Bitmap takeScreenshot() {
-		return renderer.takeScreenshot();
-	}	
-	
-	/**
-	 * 
-	 * @return the OpenGL surface.
-	 */
-	public SurfaceView getSurfaceView() {
-		return glSurfaceView;
+		return andarView.takeScreenshot();
 	}
+
+	public GLSurfaceView getSurfaceView() {
+		return this.andarView.getSurfaceView();
+	}
+
 }
---
+++ code/andar/src/edu/dhbw/andar/AndARView.java
@@ -0,0 +1,85 @@
+package edu.dhbw.andar;
+
+import java.io.IOException;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.opengl.GLSurfaceView;
+import android.view.SurfaceHolder.Callback;
+import android.view.View;
+import android.widget.FrameLayout;
+import edu.dhbw.andar.camera.CameraManager;
+import edu.dhbw.andar.exceptions.AndARRuntimeException;
+import edu.dhbw.andar.interfaces.OpenGLRenderer;
+import edu.dhbw.andar.util.IO;
+
+public class AndARView {
+
+	private GLSurfaceView glSurfaceView;
+	private PreviewSurfaceView previewSurface;
+	private AndARRenderer renderer;
+	private ARToolkit artoolkit;
+	private CameraManager cameraManager;
+
+	public View createView(Context context, Callback callback) {
+		artoolkit = new ARToolkit(context.getResources(), context.getFilesDir());
+
+		try {
+			IO.transferFilesToPrivateFS(context.getFilesDir(), context.getResources());
+		} catch (IOException e) {
+			throw new AndARRuntimeException(e.getMessage());
+		}
+		
+		FrameLayout frame = new FrameLayout(context);
+		cameraManager = new CameraManager();
+		previewSurface = new PreviewSurfaceView(context, cameraManager);
+
+		glSurfaceView = new GLSurfaceView(context);
+		renderer = new AndARRenderer(artoolkit);
+		glSurfaceView.setRenderer(renderer);
+		glSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+		glSurfaceView.getHolder().addCallback(callback);
+
+		cameraManager.setPreviewHandler(new CameraPreviewHandler(glSurfaceView, renderer, context
+				.getResources(), artoolkit, new CameraStatus()));
+
+		frame.addView(glSurfaceView);
+		frame.addView(previewSurface);
+
+		return frame;
+	}
+
+	public void pause() {
+		this.glSurfaceView.onPause();
+		this.cameraManager.pause();
+	}
+
+	public void resume() {
+		this.glSurfaceView.onResume();
+		this.cameraManager.resume();
+	}
+
+	public ARToolkit getArtoolkit() {
+		return this.artoolkit;
+	}
+
+	public void startPreview() {
+		this.previewSurface.startPreview();
+	}
+
+	public void setSurfaceCreated(boolean value) {
+		previewSurface.setSurfaceCreated(value);
+	}
+
+	public GLSurfaceView getSurfaceView() {
+		return this.glSurfaceView;
+	}
+
+	public void setNonARRenderer(OpenGLRenderer customRenderer) {
+		this.renderer.setNonARRenderer(customRenderer);
+	}
+
+	public Bitmap takeScreenshot() {
+		return renderer.takeScreenshot();
+	}
+}
