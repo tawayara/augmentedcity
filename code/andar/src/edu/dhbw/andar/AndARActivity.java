2d1c43b5e8f6236f1dfc54c002ae9b1632f12fc7
edwincarlo <edwin.carlo@gmail.com>
2014-01-05 20:15:31 -0300

    Removing some unused stuff

--- code/andar/src/edu/dhbw/andar/AndARActivity.java
+++ code/andar/src/edu/dhbw/andar/AndARActivity.java
@@ -1,28 +1,8 @@
-/**
-	Copyright (C) 2009,2010  Tobias Domhan
-
-    This file is part of AndOpenGLCam.
-
-    AndObjViewer is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    AndObjViewer is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with AndObjViewer.  If not, see <http://www.gnu.org/licenses/>.
- 
- */
 package edu.dhbw.andar;
 
 import java.lang.Thread.UncaughtExceptionHandler;
 
 import android.app.Activity;
-import android.content.pm.ActivityInfo;
 import android.graphics.Bitmap;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
@@ -33,62 +13,55 @@ import android.view.Window;
 import android.view.WindowManager;
 import edu.dhbw.andar.interfaces.OpenGLRenderer;
 
+/**
+ * This class creates the base behavior for an Activity that uses the augmented reality
+ * functionality from AndAR.
+ * 
+ * In order to work properly, it is necessary to set the orientation to landscape on the manifest
+ * file for the Activity class that will implement it.
+ */
 public abstract class AndARActivity extends Activity implements Callback, UncaughtExceptionHandler {
 
 	private AndARView andarView;
 
-	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		Thread.currentThread().setUncaughtExceptionHandler(this);
-		setFullscreen();
-		disableScreenTurnOff();
-		// orientation is set via the manifest
 
+		// Change the UI to be presented in full screen mode and avoid that the screen get's turned
+		// off by the system.
+		this.setFullscreen();
+		this.disableScreenTurnOff();
+
+		// Create the view to be presented by the Activity
 		andarView = new AndARView();
 		setContentView(andarView.createView(this, this));
 
-		if (Config.DEBUG)
+		if (Config.DEBUG) {
 			Debug.startMethodTracing("AndAR");
+		}
+	}
+
+	private void setFullscreen() {
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+	}
+
+	private void disableScreenTurnOff() {
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
+				WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 	}
 
 	/**
-	 * Set a renderer that draws non AR stuff. Optional, may be set to null or omited. and setups
+	 * Set a renderer that draws non AR stuff. Optional, may be set to null or omitted. and setups
 	 * lighting stuff.
 	 * 
 	 * @param customRenderer
 	 */
 	public void setNonARRenderer(OpenGLRenderer customRenderer) {
 		andarView.setNonARRenderer(customRenderer);
-	}
-
-	/**
-	 * Avoid that the screen get's turned off by the system.
-	 */
-	public void disableScreenTurnOff() {
-		getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
-				WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-	}
-
-	/**
-	 * Set's the orientation to landscape, as this is needed by AndAR.
-	 */
-	public void setOrientation() {
-		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-	}
-
-	/**
-	 * Maximize the application.
-	 */
-	public void setFullscreen() {
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-				WindowManager.LayoutParams.FLAG_FULLSCREEN);
-	}
-
-	public void setNoTitle() {
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
 	}
 
 	@Override
@@ -99,59 +72,34 @@ public abstract class AndARActivity extends Activity implements Callback, Uncaug
 	}
 
 	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-		System.runFinalization();
-		if (Config.DEBUG)
-			Debug.stopMethodTracing();
-	}
-
-	@Override
 	protected void onResume() {
 		andarView.resume();
 		super.onResume();
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		System.runFinalization();
+		
+		if (Config.DEBUG)
+			Debug.stopMethodTracing();
 	}
 
 	protected void startPreview() {
 		andarView.startPreview();
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see android.app.Activity#onStop()
-	 */
-	@Override
-	protected void onStop() {
-		super.onStop();
-	}
-
-	/*
-	 * The GLSurfaceView changed
-	 * 
-	 * @see android.view.SurfaceHolder.Callback#surfaceChanged(android.view.SurfaceHolder, int, int,
-	 * int)
-	 */
 	@Override
 	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
 
 	}
 
-	/*
-	 * The GLSurfaceView was created The camera will be opened and the preview started
-	 * 
-	 * @see android.view.SurfaceHolder.Callback#surfaceCreated(android.view.SurfaceHolder)
-	 */
 	@Override
 	public void surfaceCreated(SurfaceHolder holder) {
 		andarView.setSurfaceCreated(true);
 	}
 
-	/*
-	 * GLSurfaceView was destroyed The camera will be closed and the preview stopped.
-	 * 
-	 * @see android.view.SurfaceHolder.Callback#surfaceDestroyed(android.view.SurfaceHolder)
-	 */
 	@Override
 	public void surfaceDestroyed(SurfaceHolder holder) {
 
